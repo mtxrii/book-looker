2944bd20d1fe45558b8b90ebbf5bb03711126c93
@EzraMoussa <ezra.moussa@gmail.com>
2020-06-02 20:48:36 -0700

    search results update

--- book-app/src/components/home.vue
+++ book-app/src/components/home.vue
@@ -24,15 +24,58 @@
     </v-container>
     </div>
 
-  <div class="list-group-item " style="margin-left: 1vw;  overflow-y: hidden; margin-right: 1vw; margin-bottom: 1vh; " v-for="(book, key) in titles" v-bind:key="key">
+  <div class="list-group-item " style="margin-left: 1vw;  overflow-y: hidden; margin-right: 1vw; margin-bottom: 1vh; " v-for="(book, key) in loopData" v-bind:key="key">
 
-    <v-list-item-title
+    <!-- <v-list-item-title
         style="word-break: normal; padding-top: 5px; display: inline-block; font-size: 18px; max-width:45vw"
         class=" text-truncate"
         text-truncate
         v-text = "book"
         
-    > </v-list-item-title>
+    > </v-list-item-title> -->
+
+
+    <v-card
+    class="mx-auto elevation-20"
+    color="purple"
+    dark
+    style="max-width: 400px;"
+  >
+    <v-row justify="space-between">
+      <v-col cols="8">
+        <v-card-title primary-title>
+          <div>
+            <div v-text = "book.volumeInfo.title" class="headline"></div>
+            <div v-text = "book.volumeInfo.authors"></div>
+            <div v-text = "book.volumeInfo.publishedDate"></div>
+          </div>
+        </v-card-title>
+      </v-col>
+      <v-img
+        class="shrink ma-2"
+        contain
+        src="book.volumeInfo.imageLinks.thumbnail"
+        style="flex-basis: 125px"
+      ></v-img>
+    </v-row>
+    <v-divider dark></v-divider>
+    <v-card-actions class="pa-4">
+      Rate this album
+      <v-spacer></v-spacer>
+      <span class="grey--text text--lighten-2 caption mr-2">
+        <!-- ({{ rating }}) -->
+      </span>
+      <v-rating
+        v-model="rating"
+        background-color="white"
+        color="yellow accent-4"
+        dense
+        half-increments
+        hover
+        size="18"
+      ></v-rating>
+    </v-card-actions>
+  </v-card>
     
   </div>
 
@@ -60,12 +103,18 @@
         author: false,
         isbn: false,
         data: "",
+        loopData: [],
         titles: [],
         searchField: "",
         src: "",
       }
     },
     methods: {
+
+      getImage: function(path) {
+        return require(path)
+      },
+
       getRequest: function(){
 
         let splitString = this.searchField.split(" ");
@@ -77,13 +126,11 @@
             url = url + "&maxResults=40"
           }
         }
-        
+
         fetch(url)
         .then(data =>{return data.json()})
         .then(res=>{this.data = res; this.handleResponse()})
-        
         // .catch(error=>this.data = "Nothing Found!");   
-        
       },
 
       handleResponse: function() {
@@ -93,6 +140,13 @@
         var item = this.data.items[i];
         this.titles.push(item.volumeInfo.title);
       }
+      this.loopData = [];
+      for (var j = 0; j < this.data.items.length; j++) {
+        var newItem = this.data.items[j];
+        this.loopData.push(newItem);
+        console.log(newItem.volumeInfo.imageLinks.thumbnail);
+      }
+      console.log(this.loopData);
     }
 
       
