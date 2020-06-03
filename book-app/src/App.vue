84d1f80e37d4c1743befdcf811bbec843f74e537
Enrique <edavalos@ucsc.edu>
2020-06-03 02:29:57 -0700

    Added ISBN page

--- book-app/src/App.vue
+++ book-app/src/App.vue
@@ -3,7 +3,19 @@
 
 
       <div id="content">
-      <home id="home" />
+
+        <v-flex v-if="currentPage == 'Home'" sm10>
+            <v-btn text large color="#00b9be">Home</v-btn>
+            <v-btn text large v-on:click="currentPage = 'ISBN'">ISBN Gen</v-btn>
+        </v-flex>
+
+        <v-flex v-if="currentPage == 'ISBN'" sm10>
+            <v-btn text large v-on:click="currentPage = 'Home'">Home</v-btn>
+            <v-btn text large color="#00b9be">ISBN Gen</v-btn>
+        </v-flex>
+
+      <home v-if="currentPage == 'Home'" id="home" />
+      <isbn v-if="currentPage == 'ISBN'" id="isbn" />
       </div>
 
     
@@ -16,12 +28,48 @@
 
 
 import home from './components/home.vue'
+import isbn from './components/isbn.vue'
 
 export default {
   name: 'App',
 
   components: {
-    home
+    home,
+    isbn
+  },
+
+  data () {
+    return {
+      currentPage: "Home",
+    }
+  },
+  methods: {
+
+    isValidISBN: function(number_to_test) {
+      let sum = 0;
+      let num = number_to_test.toString().split("-").join("");
+      if (num.length !== 10 && num.length !== 13) {
+        return false;
+      }
+      if (isNaN(num.replace("X", "0"))) {
+        return false;
+      }
+
+      let x = 0;
+      for (var i = num.length; i > 0; i--) {
+        if (num[x] == "X") {
+          sum = sum + (i * 10);
+        }
+        else {
+          sum = sum + (i * parseInt(num[x]));
+        }
+        x++;
+      }
+
+      return (sum % 11 === 0);
+    },
+
+
   },
 
 };
